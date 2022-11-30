69c561ff489fefe3b097a77a48acf58e154cee7c
VictorPortugues07 <119353410+VictorPortugues07@users.noreply.github.com>
2022-11-30 18:15:23 -0300

    Add files via upload

---
+++ projeto.js
@@ -0,0 +1,35 @@
+let atletas = [
+ {
+   nome: "Cesar Abascal",
+   notas: [10, 9.34, 8.42, 10, 7.88]
+ },
+ {
+   nome: "Fernando Puntel",
+   notas:  [8, 10, 10, 7, 9.33]
+ },
+ {
+   nome: "Daiane Jelinsky",
+   notas: [7, 10, 9.5, 9.5, 8]
+ },
+ {
+   nome: "Bruno Castro",
+   notas: [10, 10, 10, 9, 9.5]
+ }
+];
+obterNome(){
+  return nome
+}
+notasObtidas(){
+  return notas
+}
+obterContagemDosValores(){
+  return notas.length
+}
+obterTotal(){
+  let total = 0;
+        this.notas.forEach(function(nota) {
+total += nota;
+        });
+        return this.obterTotal() / this.obterContagemDosValores();
+    }
+}
